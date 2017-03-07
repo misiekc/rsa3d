1115407b858ecec29b727f791bc07dfe81f9d2b7
Michal Ciesla <ciesla@VirtualBox>
2017-03-07 11:16:01 +0100

    init

---
+++ rsa3d/Shape.cpp
@@ -0,0 +1,18 @@
+/*
+ * Shape.cpp
+ *
+ *  Created on: 07.03.2017
+ *      Author: ciesla
+ */
+
+#include "Shape.h"
+
+Shape::Shape() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Shape::~Shape() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ rsa3d/Shape.h
@@ -0,0 +1,17 @@
+/*
+ * Shape.h
+ *
+ *  Created on: 07.03.2017
+ *      Author: ciesla
+ */
+
+#ifndef SHAPE_H_
+#define SHAPE_H_
+
+class Shape {
+public:
+	Shape();
+	virtual ~Shape();
+};
+
+#endif /* SHAPE_H_ */
