c0a58d847a63f007a8d5cdfc3f5fa5b5e6a8268d
ciesla <ciesla@VirtualBox>
2017-07-13 14:05:25 +0200

    free boundary conditions

---
+++ rsa3d/surfaces/NBoxFBC.cpp
@@ -0,0 +1,42 @@
+/*
+ * NBoxFBC.cpp
+ *
+ *  Created on: 13.07.2017
+ *      Author: ciesla
+ */
+
+#include "NBoxFBC.h"
+
+NBoxFBC::NBoxFBC(int dim, double s, double ndx, double vdx) : Surface(dim, s, ndx, vdx) {
+	// TODO Auto-generated constructor stub
+
+}
+
+NBoxFBC::~NBoxFBC() {
+	// TODO Auto-generated destructor stub
+}
+
+double NBoxFBC::getArea(){
+	return pow(this->size, this->dimension);
+}
+
+double * NBoxFBC::getTranslation(double *result, int dim, double s, double *p1, double *p2) {
+	for(int i=0; i<dim; i++)
+		result[i] = 0.0;
+	return result;
+}
+
+double * NBoxFBC::getTranslation(double *result, double *p1, double *p2) {
+		return NBoxFBC::getTranslation(result, this->dimension, this->size, p1, p2);
+	}
+
+void NBoxFBC::vector(double* v) {
+	this->vectorFreeBC(v);
+}
+
+double * NBoxFBC::getRandomPosition(double *result, RND *rnd) {
+	for(int i=0; i<this->dimension; i++)
+		result[i] = rnd->nextValue()*this->size;
+	return result;
+}
+
---
+++ rsa3d/surfaces/NBoxFBC.h
@@ -0,0 +1,29 @@
+/*
+ * NBoxFBC.h
+ *
+ *  Created on: 13.07.2017
+ *      Author: ciesla
+ */
+
+#ifndef SURFACES_NBOXFBC_H_
+#define SURFACES_NBOXFBC_H_
+
+#include "../Surface.h"
+#include "../RND.h"
+
+class NBoxFBC: public Surface {
+private:
+	static double * getTranslation(double *result, int dim, double s, double *p1, double *p2);
+
+public:
+	NBoxFBC(int dim, double s, double ndx, double vdx);
+	virtual ~NBoxFBC();
+
+	double getArea();
+	double * getTranslation(double *result, double *p1, double *p2);
+	void vector(double *v);
+	double * getRandomPosition(double * result, RND *rnd);
+
+};
+
+#endif /* SURFACES_NBOXFBC_H_ */
