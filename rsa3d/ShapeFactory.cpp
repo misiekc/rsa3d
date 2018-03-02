03877d96b3e267e47933b30b1749bf8e9ddcd54f
PKua007 <pkua.log@gmail.com>
2018-03-02 20:16:45 +0100

    initShapeClass preprocessor madness

--- rsa3d/ShapeFactory.cpp
+++ rsa3d/ShapeFactory.cpp
@@ -16,33 +16,47 @@
 Shape<RSA_SPATIAL_DIMENSION, RSA_ANGULAR_DIMENSION>* (*ShapeFactory::createShape)(RND *rnd);
 
 void ShapeFactory::initShapeClass(const std::string &sClass, const std::string &attr) {
-	if (sClass == "Sphere") {
-		Sphere<RSA_SPATIAL_DIMENSION>::initClass(attr);
-		ShapeFactory::createShape = Sphere<RSA_SPATIAL_DIMENSION>::create;
-	} else if (sClass == "OrientedCuboid") {
-        OrientedCuboid<RSA_SPATIAL_DIMENSION>::initClass(attr);
-        ShapeFactory::createShape = OrientedCuboid<RSA_SPATIAL_DIMENSION>::create;
-    }
-#if RSA_DIMENSION == 2
-    else if (sClass == "SpheroCylinder2D") {
-        SpheroCylinder2D::initClass(attr);
-        ShapeFactory::createShape = SpheroCylinder2D::create;
-    } else if (sClass == "Ellipse") {
-        Ellipse::initClass(attr);
-        ShapeFactory::createShape = Ellipse::create;
-    }
-#elif RSA_DIMENSION == 3
-	else if (sClass == "Cuboid") {
-		Cuboid::initClass(attr);
-		ShapeFactory::createShape = Cuboid::create3D;
-	} else if (sClass == "Cuboid2D") {
-		Cuboid::initClass(attr);
-		ShapeFactory::createShape = Cuboid::create2D;
-	}
-#endif
-    else {
-        std::cerr << "Unknown shape: " << sClass << " or wrong dimensions: " << RSA_SPATIAL_DIMENSION << ", " << RSA_ANGULAR_DIMENSION << std::endl;
-        exit(1);
-    }
 
+    // Shapes for any dimension, without angular dimensions
+    #if RSA_ANGULAR_DIMENSION == 0
+        if (sClass == "Sphere") {
+            Sphere<RSA_SPATIAL_DIMENSION>::initClass(attr);
+            ShapeFactory::createShape = Sphere<RSA_SPATIAL_DIMENSION>::create;
+            return;
+        } else if (sClass == "OrientedCuboid") {
+            OrientedCuboid<RSA_SPATIAL_DIMENSION>::initClass(attr);
+            ShapeFactory::createShape = OrientedCuboid<RSA_SPATIAL_DIMENSION>::create;
+            return;
+        }
+    #endif
+
+    // Shapes of specific dimensions
+    #if RSA_SPATIAL_DIMENSION == 2
+        // 2D shapes with angular dimension
+        #if RSA_ANGULAR_DIMENSION == 1
+            if (sClass == "SpheroCylinder2D") {
+                SpheroCylinder2D::initClass(attr);
+                ShapeFactory::createShape = SpheroCylinder2D::create;
+                return;
+            } else if (sClass == "Ellipse") {
+                Ellipse::initClass(attr);
+                ShapeFactory::createShape = Ellipse::create;
+                return;
+            }
+        #endif
+
+    #elif RSA_SPATIAL_DIMENSION == 3
+        if (sClass == "Cuboid") {
+            Cuboid::initClass(attr);
+            ShapeFactory::createShape = Cuboid::create3D;
+            return;
+        } else if (sClass == "Cuboid2D") {
+            Cuboid::initClass(attr);
+            ShapeFactory::createShape = Cuboid::create2D;
+            return;
+        }
+    #endif
+
+    std::cerr << "Unknown shape: " << sClass << " or wrong dimensions: " << RSA_SPATIAL_DIMENSION << ", " << RSA_ANGULAR_DIMENSION << std::endl;
+    exit(EXIT_FAILURE);
 }
