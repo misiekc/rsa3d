569760e7798ba82036b7ec518c589ce9776722a2
PKua007 <pkua.log@gmail.com>
2018-03-14 16:08:04 +0100

    Rectangle tests

--- test/all_ex2d_tests.sh
+++ test/all_ex2d_tests.sh
@@ -6,3 +6,5 @@ cd `dirname $0` # change working dir to script dir
 ./spheroc_pitest/test.sh
 ./ellipse_inttest/test.sh
 ./ellipse_pitest/test.sh
+./rectangle_inttest/test.sh
+./rectangle_pitest/test.sh
