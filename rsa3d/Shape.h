06b382c6b5944febf78de45d06505a754a8336b3
PKua007 <pkua.log@gmail.com>
2018-04-06 16:49:38 +0200

    Shape Doxygen documentation

--- rsa3d/Shape.h
+++ rsa3d/Shape.h
@@ -21,14 +21,34 @@ class Shape : public Positioned<SPATIAL_DIMENSION>{
 
 private:
 
-	static double voxelAngularSize;
+    static double voxelAngularSize;
 
     std::array<double, ANGULAR_DIMENSION> orientation;
 
 protected:
 
+    /**
+     * Translates second by a vector returned by
+     * \code
+     * bc->getTranslation(result, this, second);
+     * \endcode
+     * @param bc boundary conditions to apply
+     * @param second shape to be translated according to bc
+     */
 	void applyBC(BoundaryConditions *bc, Shape<SPATIAL_DIMENSION, ANGULAR_DIMENSION> *second) const;
 
+    /**
+     * Sets shape's orientation. Derived shape classes have to override this method when they want to keep track of
+     * shape's orientation. One would then typically write:
+     * \code
+     * void Derived::setOrientation(const double *orientation) {
+     *     Shape::setOrientation(orientation);
+     *     // invoke getOrientation and for example compute vertices
+     * }
+     * \endcode
+     * rotate(double*) method delegates to this, so no orientation changes will be missed.
+     * @param orientation new shape's orientation
+     */
     virtual void setOrientation(const double *orientation);
 
 public:
@@ -37,61 +57,119 @@ public:
     int no;
 
 	double time;
+
     Shape();
-
 	Shape(const Shape<SPATIAL_DIMENSION, ANGULAR_DIMENSION> & other);
-
 	virtual ~Shape();
 
-	// returns linear size of a cell in a NeighbourGrig. This size should be as small as possible but big enough to avoid overlapping between shapes having centers in cells that are not neighbours
+    /**
+     * Returns linear size of a cell in a NeighbourGrid. This size should be as small as possible but big enough to
+     * avoid overlapping between shapes having centers in cells that are not neighbours.
+     * @return linear size of a cell in a NeighbourGrid
+     */
 	virtual double getNeighbourListCellSize() const = 0;
 
-	// returns initial linear size of a (cubic) voxel. This size should be as big as possible but shape with the center inside the voxel have to cover the whole voxel
+    /**
+     * Returns initial linear size of a (cubic) voxel. This size should be as big as possible but shape with the center
+     * inside the voxel have to cover the whole voxel.
+     * @return initial linear size of a (cubic) voxel
+     */
 	virtual double getVoxelSize() const = 0;
 
+    /**
+     * Returns angular size of a voxel. This should be as small as possible but big enough so that angle range
+     * (0, `getVoxelAngularSize()`) describes all possible shape's orientations.
+     * @return angular size of a voxel
+     */
 	virtual double getVoxelAngularSize() const;
+
+    /**
+     * Returns an array of all angles describing shape's orientation.
+     * @return array describing shape's orietation
+     */
     const double * getOrientation() const;
 
-	// translates the shape by the given vector v
+	/**
+	 * Translates the shape by a given vector v.
+	 * @param v a vector to translate
+	 */
 	void translate(double* v);
 
+    /**
+     * Increases all shape's angles by respective values from array v.
+     * @param v an array of angle deltas
+     */
 	void rotate(double *v);
 
-	// checks if there is overlap with the shape pointed by s.
+	/**
+	 * Checks if there is overlap with the shape pointed by s.
+	 * @param bc boundary conditions to take into account
+	 * @param s the second shape
+	 * @return 0 if there is no overlap, nonzero number otherwise
+	 */
 	virtual int overlap(BoundaryConditions *bc, Shape<SPATIAL_DIMENSION, ANGULAR_DIMENSION> *s) const = 0;
 
-	// returns a volume of the shape
+    /**
+     * Returns a volume of the shape.
+     * @return a volume of the shape
+     */
 	virtual double getVolume() const = 0;
 
-	// checks if a given point is within excluded volume
+    /**
+     * Checks if a given point is within intersection of all excluded volumes for all orientations denoted by
+     * orientation and orientationRange parameters.
+     * @param bc boundary conditions to take into account
+     * @param position position of a point to check
+     * @param orientation array of beginnings of angle intervals
+     * @param orientationRange array of lengths of angle intervals
+     * @return 0 if point is outside, nonzero number otherwise
+     */
 	virtual int pointInside(BoundaryConditions *bc, double* position, double *orientation, double orientationRange) const = 0;
 
-	// checks if a given point is within excluded volume for any orientation of a virtual rotationally symmetric shape
+    /**
+     * Checks if a given point is within excluded volume for any orientation of a virtual rotationally symmetric shape.
+     * @param bc boundary conditions to take into account
+     * @param da position of a point to check
+     * @return 0 if point is outside, nonzero number otherwise
+     */
 	virtual int pointInside(BoundaryConditions *bc, double* da) const;
 
-	// moves the shape towards given shape s
+    /**
+     * ?? Moves the shape towards given shape s.
+     * @param s ??
+     * @return ??
+     */
 	virtual double minDistance(Shape<SPATIAL_DIMENSION, ANGULAR_DIMENSION> *s) const;
 
-	// returns string representation of the shape
+    /**
+     * Returns string representation of the shape.
+     * @return string representation of the shape
+     */
 	virtual std::string toString() const;
 
-	// returns povray string representation of the shape
+    /**
+     * Returns povray string representation of the shape.
+     * @return povray string representation of the shape
+     */
 	virtual std::string toPovray() const;
 
-	// returns Wolfram Mathematica string representation of the shape
+    /**
+     * Returns Wolfram Mathematica string representation of the shape.
+     * @return Wolfram Mathematica string representation of the shape
+     */
 	virtual std::string toWolfram() const;
 
-	// serialize shape
+    /**
+     * Serializes shape and writes it in binary form to f.
+     * @param f stream to write serialized shape to
+     */
 	virtual void store(std::ostream &f) const;
 
-	// deserialize shape
+	/**
+	 * Deserializes shape from f.
+	 * @param f stream to write serialized shape from
+	 */
 	virtual void restore(std::istream &f);
-
-	// draws the shape
-    // virtual void draw()
-
-//    void vectorTranslate(const Vector<SPATIAL_DIMENSION, ANGULAR_DIMENSION> &translation);
-
 };
 
 #include "Shape.tpp"
