f0b8f073d17fda3ac2a964dfcaec299a61bf5de5
Piotr Kubala <pkua.log@gmail.com>
2019-05-17 02:08:26 +0200

    only necessary sources compilation for specific dimension

--- stat_test/Main.cpp
+++ stat_test/Main.cpp
@@ -3,28 +3,28 @@
 //
 
 #include <cstdlib>
+
 #include "../rsa3d/utils/Utils.h"
-#include "CuboidSpeedTest.h"
 #include "ShapeOverlapTest.h"
 #include "ShapePointInsideTest.h"
 #include "ShapeStaticSizesTest.h"
 #include "AnisotropicShape2DExclusionTest.h"
 #include "VectorSpeedTest.h"
 #include "AnisotropicShape2DExclusionDrawer.h"
-#include "RectangleCase.h"
 #include "PackingOverlapsTest.h"
-#include "OrderParamTest.h"
 #include "ShapeBCTest.h"
 
+#if RSA_SPATIAL_DIMENSION == 3 && RSA_ANGULAR_DIMENSION == 0
+    #include "CuboidSpeedTest.h"
+    #include "OrderParamTest.h"
+#endif
 
 int main(int argc, char **argv) {
     if (argc < 2)
         die("No mode param. Aborting.");
 
     std::string mode(argv[1]);
-    if (mode == "cube_speedtest")
-        return cube_speedtest::main(argc, argv);
-    else if (mode == "shape_ovtest")
+    if (mode == "shape_ovtest")
         return shape_ovtest::main(argc, argv);
     else if (mode == "shape_pitest")
         return shape_pitest::main(argc, argv);
@@ -38,12 +38,15 @@ int main(int argc, char **argv) {
         return vec_speedtest::main(argc, argv);
     else if (mode == "as2d_exdrawer")
         return as2d_exdrawer::main(argc, argv);
-    else if (mode == "rectangle_case")
-    	return rectanglecase::main(argc, argv);
     else if (mode == "pack_ovtest")
         return pack_ovtest::main(argc, argv);
-    else if (mode == "order_param_test")
-        return order_param_test::main(argc, argv);
+
+    #if RSA_SPATIAL_DIMENSION == 3 && RSA_ANGULAR_DIMENSION == 0
+        if (mode == "cube_speedtest")
+            return cube_speedtest::main(argc, argv);
+        else if (mode == "order_param_test")
+            return order_param_test::main(argc, argv);
+    #endif
 
     std::cerr << "Unknown test mode: " << mode << std::endl;
     return EXIT_FAILURE;
