7bd55d15c5fe5a77622e5ae6fb43ec2561d9cabf
Piotr Kubala <pkua.log@gmail.com>
2019-05-27 18:07:39 +0200

    ProgramArguments stdin test

--- rsa3d/ProgramArguments.cpp
+++ rsa3d/ProgramArguments.cpp
@@ -19,7 +19,7 @@ namespace {
         void readFromFile(const std::string &filename, std::ostream &output) override {
             std::ifstream inputFile(filename);
             if (!inputFile)
-                throw InvalidArgumentsException("Cannot open input file " + filename + "to read. Aborting.");
+                throw InvalidArgumentsException("Cannot open input file " + filename + " to read. Aborting.");
 
             output << inputFile.rdbuf() << std::endl;
         }
@@ -28,16 +28,12 @@ namespace {
 
 ProgramArguments::ProgramArguments(int argc, char **argv) {
     ParametersFileReaderImpl pfr;
-    *this = ProgramArguments(argc, argv, pfr);
+    *this = ProgramArguments(argc, argv, pfr, std::cin);
 }
 
-ProgramArguments::ProgramArguments(int argc, char **argv, ParametersFileReader &pfr) {
+ProgramArguments::ProgramArguments(int argc, char **argv, ParametersFileReader &pfr, std::istream &inputStream) {
     Expects(argc >= 1);     // valid program arguments have at least 1 arg - cmd; assertion error otherwise
     cmd = argv[0];
-
-    // Now valid program arguments, we use InvalidArgumentsException from now on
-    if (argc < 2)
-        throw InvalidArgumentsException("Usage: " + cmd + " <mode> (additional parameters)");
 
     std::stringstream input;
     for (int i = 1; i < argc; i++) {
@@ -48,7 +44,7 @@ ProgramArguments::ProgramArguments(int argc, char **argv, ParametersFileReader &
             pfr.readFromFile(argv[i], input);
             input << std::endl;
         } else if (arg == "-i") {
-            input << std::cin.rdbuf() << std::endl;
+            input << inputStream.rdbuf() << std::endl;
         } else if (startsWithMinus(arg)) {
             input << arg.substr(1) << std::endl;
         } else {
@@ -65,6 +61,10 @@ bool ProgramArguments::startsWithMinus(const std::string &arg) const {
 }
 
 void ProgramArguments::fetchModeArgument() {
+    // First positional argument, after stripping all extra stuff, should be the mode parameter
+    if (positionalArguments.empty())
+        throw InvalidArgumentsException("Usage: " + cmd + " <mode> (additional parameters)");
+
     mode = positionalArguments[0];
     positionalArguments.erase(positionalArguments.begin());
 }
