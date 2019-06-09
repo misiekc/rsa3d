5ea2d79a4b84f918eebd9ddd2e9a2c3410b13397
Piotr Kubala <pkua.log@gmail.com>
2019-06-09 22:29:16 +0200

    more Quantity tests

--- unit_test/rsa3d/utils/QuantityTest.cpp
+++ unit_test/rsa3d/utils/QuantityTest.cpp
@@ -7,69 +7,84 @@
 #include "../../../rsa3d/utils/Quantity.h"
 
 TEST_CASE("Quantity: rounding based on error") {
-    SECTION("decimal with error with fractional part") {
-        std::ostringstream result;
+    SECTION("basic") {
+        SECTION("decimal with error with fractional part") {
+            std::ostringstream result1, result2;
 
-        result << Quantity(34.344, 2.559);
+            result1 << Quantity(34.344, 2.559);
+            result2 << Quantity(234.234544, 0.03425);
 
-        REQUIRE(result.str() == "34.34\t2.56");
+            REQUIRE(result1.str() == "34.34\t2.56");
+            REQUIRE(result2.str() == "234.2345\t0.0343");
+        }
+
+        SECTION("negative decimal") {
+            std::ostringstream result;
+
+            result << Quantity(-34.344, 2.559);
+
+            REQUIRE(result.str() == "-34.34\t2.56");
+        }
+
+        SECTION("decimal with error touching fractional part") {
+            std::ostringstream result;
+
+            result << Quantity(1345.34, 127.556);
+
+            REQUIRE(result.str() == "1345\t128");
+        }
+
+        SECTION("decimal with error not touching fractional part") {
+            std::ostringstream result;
+
+            result << Quantity(28345.34, 1277.556);
+
+            REQUIRE(result.str() == "28350\t1280");
+        }
     }
 
-    SECTION("negative decimal") {
-        std::ostringstream result;
+    SECTION("tricky") {
+        SECTION("decimal with larger error") {
+            std::ostringstream result1, result2;
 
-        result << Quantity(-34.344, 2.559);
+            result1 << Quantity(445.6, 2256.5);
+            result2 << Quantity(0.4563, 1.567);
 
-        REQUIRE(result.str() == "-34.34\t2.56");
+            REQUIRE(result1.str() == "450\t2260");
+            REQUIRE(result2.str() == "0.46\t1.57");
+        }
+
+        SECTION("decimal with much larger error") {
+            std::ostringstream result;
+
+            result << Quantity(54.62, 256332.5);
+
+            REQUIRE(result.str() == "50\t256000");
+        }
+
+        SECTION("decimal with zeros in value") {
+            std::ostringstream result;
+
+            result << Quantity(45.6, 0.0002332);
+
+            REQUIRE(result.str() == "45.600000\t0.000233");
+        }
+
+        SECTION("decimal with zeros in error") {
+            std::ostringstream result;
+
+            result << Quantity(45.6, 0.0002);
+
+            REQUIRE(result.str() == "45.600000\t0.000200");
+        }
     }
 
-    SECTION("decimal with error touching fractional part") {
+    SECTION("std::fixed isolation") {
         std::ostringstream result;
+        result << std::fixed;
 
-        result << Quantity(1345.34, 127.556);
+        result << Quantity(234.234544, 0.03425);
 
-        REQUIRE(result.str() == "1345\t128");
-    }
-
-    SECTION("decimal with error not touching fractional part") {
-        std::ostringstream result;
-
-        result << Quantity(28345.34, 1277.556);
-
-        REQUIRE(result.str() == "28350\t1280");
-    }
-
-    SECTION("decimal with larger error") {
-        std::ostringstream result1, result2;
-
-        result1 << Quantity(445.6, 2256.5);
-        result2 << Quantity(0.4563, 1.567);
-
-        REQUIRE(result1.str() == "450\t2260");
-        REQUIRE(result2.str() == "0.46\t1.57");
-    }
-
-    SECTION("decimal with much larger error") {
-        std::ostringstream result;
-
-        result << Quantity(54.62, 256332.5);
-
-        REQUIRE(result.str() == "50\t256000");
-    }
-
-    SECTION("decimal with zeros in value") {
-        std::ostringstream result;
-
-        result << Quantity(45.6, 0.0002332);
-
-        REQUIRE(result.str() == "45.600000\t0.000233");
-    }
-
-    SECTION("decimal with zeros in error") {
-        std::ostringstream result;
-
-        result << Quantity(45.6, 0.0002);
-
-        REQUIRE(result.str() == "45.600000\t0.000200");
+        REQUIRE(result.str() == "234.2345\t0.0343");
     }
 }
