6307b0881df12573a3bb94d97a0921ff57ee3ebe
Piotr Kubala <pkua.log@gmail.com>
2020-10-17 20:53:43 +0200

    FlatSurfaceFunction implementation

---
+++ rsa3d/surface_functions/FlatSurfaceFunction.h
@@ -0,0 +1,25 @@
+//
+// Created by Piotr Kubala on 17/10/2020.
+//
+
+#ifndef RSA3D_FLATSURFACEFUNCTION_H
+#define RSA3D_FLATSURFACEFUNCTION_H
+
+#include "../SurfaceFunction.h"
+#include "../utils/Utils.h"
+#include "../utils/Assertions.h"
+
+class FlatSurfaceFunction : public SurfaceFunction {
+public:
+    [[nodiscard]] MinMax calculateValueRange(const RSAVector &voxelPosition, double voxelSpatialSize) const override {
+        return {0, 0};
+    }
+
+    void fillInLastCoordinate(RSAVector &position) const override {
+        Assert(RSA_SPATIAL_DIMENSION > 1);
+        position[RSA_SPATIAL_DIMENSION - 1] = 0;
+    }
+};
+
+
+#endif //RSA3D_FLATSURFACEFUNCTION_H
